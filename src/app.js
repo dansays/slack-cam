9624729f4322b1ae556392f487c480ad1dbd99c0
Dan Budiac <dansays@gmail.com>
2017-04-18 17:16:27 -0400

    Add zoom, compression

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ let sound         = require('play-sound')();
 let config        = require('./config');
 config.delay      = config.delay || 2.5;
 config.frequency  = config.frequency || 5;
+config.zoom       = config.zoom || 450;
 
 // Create a new cam instance;
 let cam = nodecam.create({
 	callbackReturn  : 'buffer'
+, output          : 'png'
 , verbose         : false
 , device          : config.device
 , delay           : config.delay
@@ -28,22 +30,27 @@ async function captureImage() {
 
 	let buffer;
 
+	// Play a sound a few seconds before capture
 	console.log('Here we go...')
 	console.log('...say cheese!')
-	await beep(config.delay);
+	sound.play('shutter.mp3');
 
-	// First we grab an image from the webcam
+	// Grab an image from the webcam
 	console.log('...capturing image');
 	try {	buffer = await capture(); }
 	catch (err) { console.error(err); }
 
-	// Raw images are way too big. Let's resize it,
-	// and crop it down to a square.
-	console.log('...resizing and cropping');
-	try { buffer = await resize(buffer); }
-	catch (err) { console.error(err); }
+	// Zoom!
+	console.log('...squishing vertically');
+	try { buffer = await zoom(buffer); }
+	catch (err) { console.log(err); }
 
-	// Lastly, let's send the new image to Slack.
+	// Crop!
+	console.log('...cropping horizontally');
+	try { buffer = await crop(buffer); }
+	catch (err) { console.log(err); }	
+
+	// Send the new image to Slack.
 	console.log('...uploading to Slack');
 	try { await upload(buffer); }
 	catch (err) { console.error(err); }
@@ -53,15 +60,6 @@ async function captureImage() {
 	console.log('');
 
 	//////////////////////////////////////////////////////////////
-
-	// Play a shutter sound before taking a picture
-	async function beep(delay) {
-		return new Promise((resolve, reject) => {
-			sound.play('shutter.mp3');
-			setTimeout(() => resolve(), delay * 1000);
-		});
-	}
-
 
 	// Grab an image from the webcam
 	async function capture() {
@@ -73,18 +71,21 @@ async function captureImage() {
 		});
 	}
 
-	// Resize and crop the image
-	async function resize(buffer) {
-		return new Promise((resolve, reject) => {
-			sharp(buffer)
-				.normalize()
-				.crop(sharp.strategy.entropy)
-				.resize(480, 480)
-				.toBuffer((err, buffer) => {
-					if (err) reject(err)
-					else resolve(buffer);
-				});
-		});
+	// Crop the image vertically, to create a zoom effect
+	async function zoom(buffer) {
+		return sharp(buffer)
+			.crop(sharp.gravity.center)
+			.resize(1280, config.zoom)
+			.toBuffer();
+	}
+
+	// Crop the image into a square, so it's avatar-shaped
+	async function crop(buffer) {
+		return sharp(buffer)
+			.crop(sharp.strategy.entropy)
+			.resize(500, 500)
+			.jpeg({ quality: 40 })
+			.toBuffer();
 	}
 
 	// Upload the image to Slack
